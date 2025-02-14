2f4966108152d994797ec6ec5efd8278da4fff60
ilana <igershteyn@gmail.com>
2025-02-14 14:14:28 -0800

    add dynamic port

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import router
 from app.config import load_env
+import os
 
 # Load environment variables
 load_env()
@@ -12,4 +13,5 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Use Heroku's assigned port or default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
